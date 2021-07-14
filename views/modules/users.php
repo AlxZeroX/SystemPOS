c51ed5c6ba456fce16a68bce0131e1dadb4fd737
Cristian_Martinez <thealxzerox@gmail.com>
2021-07-13 22:38:01 -0600

    Config Users

--- views/modules/users.php
+++ views/modules/users.php
@@ -6,7 +6,7 @@
       <small>Control Panel</small>
     </h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li><a href="home#"><i class="fa fa-dashboard"></i> Home</a></li>
       <li class="active">Manage Users</li>
     </ol>
 
